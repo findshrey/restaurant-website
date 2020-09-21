4a75f8a72f0d239f3f42dbe8b68bf2a6d242c9de
shrey9496 <shrey9april@gmail.com>
2020-09-21 19:30:30 +0530

    Migrated to React

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+module.exports = {
+   entry: './src/index.js',
+   output: {
+      path: path.resolve(__dirname, 'public/scripts'),
+      filename: 'bundle.js'
+   },
+   module: {
+      rules: [{
+         test: /\.js$/,
+         exclude: /node_modules/,
+         use: {
+            loader: 'babel-loader',
+            // options: {
+            //    presets: ['@babel/preset-env', '@babel/preset-react']
+            // }
+         }
+      }, {
+         test: /\.scss$/,
+         use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+         ]
+      }]
+   },
+   devServer: {
+      contentBase: path.resolve(__dirname, 'public'),
+      publicPath: '/scripts/'
+   },
+   devtool: 'cheap-module-eval-source-map'
+}
