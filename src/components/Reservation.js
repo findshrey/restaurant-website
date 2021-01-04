23e0e05f97a430f1d3fcc2bf43a54a64cb2ca065
shrey9496 <shrey9april@gmail.com>
2021-01-05 05:01:29 +0530

    Refactored Reservation section

--- src/components/Reservation.js
+++ src/components/Reservation.js
@@ -1,27 +1,27 @@
 import React from 'react'
 
 const Reservation = () => (
-   <section id="reservation">
+   <section id="reservation" className="reservation">
       <div className="container">
          <header className="head-white">
             <h4>Book your table</h4>
             <h2>Reservation</h2>
          </header>
-         <form>
-            <div className="form-wrapper">
-               <div className="form-group">
-                  <input type="text" placeholder="Date*" required />
-                  <input type="text" placeholder="Time*" required />
-                  <input type="number" placeholder="Party" />
-               </div>
-               <div className="form-group">
-                  <input type="text" placeholder="Name*" required />
-                  <input type="number" placeholder="Phone*" required />
-                  <input type="email" placeholder="Email" />
-               </div>
-               <textarea name="" id="" rows="13" placeholder="Message" />
+         <form className="form">
+            <div className="form-group">
+               <input type="text" placeholder="Date*" required />
+               <input type="text" placeholder="Time*" required />
+               <input type="number" placeholder="Party" />
             </div>
-            <button type="submit" className="btn-reserve">Make Reservation</button>
+            <div className="form-group">
+               <input type="text" placeholder="Name*" required />
+               <input type="number" placeholder="Phone*" required />
+               <input type="email" placeholder="Email" />
+            </div>
+            <textarea rows="13" placeholder="Message" />
+            <div className="btn-wrapper">
+               <button type="submit" className="btn-reserve">Make Reservation</button>
+            </div>
          </form>
       </div>
    </section>
